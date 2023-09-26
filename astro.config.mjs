76d8831a0b2734c9146434f864fd7aec24aa5a4c
Raul de Melo <melo.raulf@gmail.com>
2023-09-26 10:47:00 +0200

    add a workaround

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    ssr: {
+      /**
+       * Workaround. Uncomment the `noExternal` line to fix the error:
+       * @see https://vite-plugin-ssr.com/broken-npm-package
+       */
+      // noExternal: ["lil-gui"],
+    },
+  },
+});
